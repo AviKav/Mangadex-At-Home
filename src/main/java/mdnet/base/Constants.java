8ba1c6dd2fc9fe45cbafc6688a7fe1d8aa23fd2a
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2020-06-11 22:05:40 -0500

    Bump build version

--- src/main/java/mdnet/base/Constants.java
+++ src/main/java/mdnet/base/Constants.java
@@ -3,7 +3,7 @@ package mdnet.base;
 import java.time.Duration;
 
 public class Constants {
-	public static final int CLIENT_BUILD = 2;
+	public static final int CLIENT_BUILD = 3;
 	public static final String CLIENT_VERSION = "1.0";
 	public static final Duration MAX_AGE_CACHE = Duration.ofDays(14);
 }
