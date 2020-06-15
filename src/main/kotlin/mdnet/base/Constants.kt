fa4f89b70651d38b31ff0ce73f7f25a9e32ea074
Amos Ng <amosng1@gmail.com>
2020-06-16 06:31:54 +0800

    Bumped CLIENT_BUILD in preparation for RC17

--- src/main/kotlin/mdnet/base/Constants.kt
+++ src/main/kotlin/mdnet/base/Constants.kt
@@ -3,7 +3,7 @@ package mdnet.base
 import java.time.Duration
 
 object Constants {
-    const val CLIENT_BUILD = 6
+    const val CLIENT_BUILD = 7
     const val CLIENT_VERSION = "1.0"
     val MAX_AGE_CACHE: Duration = Duration.ofDays(14)
 }
