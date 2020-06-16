d27cd9194762ba088916b6b3229ba27dbdd3e086
Fugi <fugiman47@gmail.com>
2020-06-15 22:59:35 -0700

    Also set Access-Control-Expose-Headers

--- src/main/kotlin/mdnet/base/server/ImageServer.kt
+++ src/main/kotlin/mdnet/base/server/ImageServer.kt
@@ -233,6 +233,7 @@ class ImageServer(private val cache: DiskLruCache, private val statistics: Atomi
                 listOf("public", MaxAgeTtl(Constants.MAX_AGE_CACHE).toHeaderValue()).joinToString(", ")
             )
             .header("Access-Control-Allow-Origin", "https://mangadex.org")
+            .header("Access-Control-Expose-Headers", "*")
             .header("Timing-Allow-Origin", "https://mangadex.org")
             .let {
                 if (length != null) {
