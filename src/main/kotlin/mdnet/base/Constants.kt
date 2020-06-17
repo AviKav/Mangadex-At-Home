1c3fa630dacf828649b50c1a27f8c325a1ecca33
RedMatriz <myyl815@gmail.com>
2020-06-17 22:46:29 +0000

    Webui

--- src/main/kotlin/mdnet/base/Constants.kt
+++ src/main/kotlin/mdnet/base/Constants.kt
@@ -5,5 +5,6 @@ import java.time.Duration
 object Constants {
     const val CLIENT_BUILD = 8
     const val CLIENT_VERSION = "1.0"
+    const val WEBUI_VERSION = "0.0.4"
     val MAX_AGE_CACHE: Duration = Duration.ofDays(14)
 }
---
+++ src/main/resources/webui/precache-manifest.b7ba13b46eefa1a4adbdbd28f90546f7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42dc685cf939e8cb7a0e",
+    "url": "/css/app.e2aa2c39.css"
+  },
+  {
+    "revision": "b2aeed3dfb80b677859c",
+    "url": "/css/chunk-vendors.21a2845f.css"
+  },
+  {
+    "revision": "2c125ba699a0b75ad28728c89a6a4659",
+    "url": "/index.html"
+  },
+  {
+    "revision": "42dc685cf939e8cb7a0e",
+    "url": "/js/app.dfb0f485.js"
+  },
+  {
+    "revision": "b2aeed3dfb80b677859c",
+    "url": "/js/chunk-vendors.f7ffc240.js"
+  },
+  {
+    "revision": "8acb3b80fd7f13474f9b2cb074bca31a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
