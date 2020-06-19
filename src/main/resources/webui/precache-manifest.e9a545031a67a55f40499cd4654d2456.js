0dbb793cd2eb137193402cfffdee27954c83185d
M <myyl815@gmail.com>
2020-06-18 19:35:01 -0500

    Fix WebUI reliability score issue

---
+++ src/main/resources/webui/precache-manifest.e9a545031a67a55f40499cd4654d2456.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2babbc8ffbf145a6d290",
+    "url": "/css/app.e2aa2c39.css"
+  },
+  {
+    "revision": "a78a87eb753feb7eaa01",
+    "url": "/css/chunk-vendors.ff325645.css"
+  },
+  {
+    "revision": "5115a7022a551a8e9dba815e0b07e36a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2babbc8ffbf145a6d290",
+    "url": "/js/app.74a1c55f.js"
+  },
+  {
+    "revision": "a78a87eb753feb7eaa01",
+    "url": "/js/chunk-vendors.71e890e8.js"
+  },
+  {
+    "revision": "8acb3b80fd7f13474f9b2cb074bca31a",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
