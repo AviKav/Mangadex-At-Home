28418c63158ca515e1c32f909559aa94546e34db
Amos Ng <amosng1@gmail.com>
2020-06-19 09:05:08 +0800

    Update Web-UI version number in Constants.kt

--- src/main/kotlin/mdnet/base/Constants.kt
+++ src/main/kotlin/mdnet/base/Constants.kt
@@ -7,7 +7,7 @@ import java.time.Duration
 object Constants {
     const val CLIENT_BUILD = 9
     const val CLIENT_VERSION = "1.0"
-    const val WEBUI_VERSION = "0.1.0"
+    const val WEBUI_VERSION = "0.1.1"
     val MAX_AGE_CACHE: Duration = Duration.ofDays(14)
     @JvmField
     val GSON: Gson = GsonBuilder().setPrettyPrinting().create()
