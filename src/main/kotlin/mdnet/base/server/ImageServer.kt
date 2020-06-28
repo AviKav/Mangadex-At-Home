00e4ac20bb65e9bd06c51c035294488f7523598a
Amos Ng <amosng1@gmail.com>
2020-06-29 06:08:00 +0800

    Added rudimentary support of Referer checking to mitigate hotlinking

--- src/main/kotlin/mdnet/base/server/ImageServer.kt
+++ src/main/kotlin/mdnet/base/server/ImageServer.kt
@@ -101,8 +101,12 @@ class ImageServer(private val cache: DiskLruCache, private val statistics: Atomi
             }
         }
 
+        val referer = request.header("Referer")
+
         handled.set(true)
-        if (snapshot != null && imageDatum != null) {
+        if (referer != null && !referer.contains("mangadex.org")) {
+            Response(Status.FORBIDDEN)
+        } else if (snapshot != null && imageDatum != null) {
             request.handleCacheHit(sanitizedUri, getRc4(rc4Bytes), snapshot, imageDatum)
                 .header("X-Uri", sanitizedUri)
         } else {
