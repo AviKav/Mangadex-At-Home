76d19d7493868c92d49465d0fd922c9375365363
carbotaniuman <41451839+carbotaniuman@users.noreply.github.com>
2020-07-04 10:54:00 -0500

    Goddangit

--- src/main/kotlin/mdnet/base/Constants.kt
+++ src/main/kotlin/mdnet/base/Constants.kt
@@ -22,7 +22,7 @@ import java.time.Duration
 
 object Constants {
     const val CLIENT_BUILD = 14
-    const val CLIENT_VERSION = "1.0.0"
+    const val CLIENT_VERSION = "1.1.1"
     const val WEBUI_VERSION = "0.1.1"
     val MAX_AGE_CACHE: Duration = Duration.ofDays(14)
 
